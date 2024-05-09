b875150a56ba6b304172b4d3fe43b7cfb2ad8d80
Oleg Shcherbakov <59744113+FrostHoll@users.noreply.github.com>
2024-05-09 20:05:37 +0500

    09.05 Scripting - 02

--- Assets/Scripts/Script.cs
+++ Assets/Scripts/Script.cs
@@ -4,28 +4,37 @@ using UnityEngine;
 
 public class Script : MonoBehaviour
 {
-    int health = 10;
+    bool isSelected = false;
 
-    void OnMouseEnter()
+    float speed = 5f;
+
+    Light halo;
+
+    void Start()
     {
-        health = 10;
-        Debug.Log("Здоровье восстановлено!");
+        halo = GetComponent<Light>();
+        halo.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) && isSelected)
+        {
+            transform.Translate(speed * Time.deltaTime * Vector3.left);
+        }
+        if (Input.GetKey(KeyCode.D) && isSelected)
+        {
+            transform.Translate(speed * Time.deltaTime * Vector3.right);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isSelected)
+        {
+            Instantiate(gameObject, transform.position + Random.onUnitSphere * 3f, Quaternion.identity);
+        }
     }
 
     void OnMouseDown()
     {
-        if (health > 0)
-        {
-            health--;
-            if (health == 0)
-                Debug.Log("Победа!");
-            else 
-                Debug.Log($"HP: {health}");
-        }
-    }
-
-    void OnMouseExit()
-    {
-        Debug.Log($"Оставшееся здоровье: {health}");
+        isSelected = !isSelected;
+        halo.enabled = isSelected;
     }
 }
